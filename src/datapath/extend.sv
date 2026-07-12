fa01fd8ccb42f8c33adf6c49c286f5c889a62afc
Morgan Bleasdale <morganbleasdale234@gmail.com>
2026-07-12 15:17:50 +0100

    EXTEND - comments explaining EXT instruction

--- src/datapath/extend.sv
+++ src/datapath/extend.sv
@@ -1,3 +1,25 @@
+/*
+The max input size for an IMM is 8-bits.
+Registers are 16-bits, so the upper 8-bits of any input IMM must be extended to 16-bits.
+
+Normally sign extension is done: 
+    0x00 -> 0x0000
+    0x01 -> 0x0001
+    0x7F -> 0x007F
+    0x80 -> 0xFF80
+    0xFF -> 0xFFFF
+MSB of IMM is copyed to the upper 8-bits of IMMEXT
+
+An EXT instruction can be used, with an 8-bit input, to set the upper 8 bits of the next input.
+Eg:
+    EXT 0xAB (cycle 1)
+    IMM 0xCD (cycle 2)
+    IMMEXT = 0xABCD (cycle 2)
+
+A register is used to hold the upper 8-bits from the EXT instruction, and is used to extend the IMM on the next cycle.
+
+*/
+
 module EXTEND (
     
     input logic clk,
